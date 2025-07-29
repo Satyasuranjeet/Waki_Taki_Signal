edad8ce6a00c1af0cfaced15a48c072a3b0a32e3
Satyasuranjeet <satyajena911@gmail.com>
2025-07-29 21:30:12 +0530

    added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_cors import CORS
 import os
@@ -9,8 +9,19 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
-# Store room information
+# Store room information and user sessions
 rooms = {}
+user_sessions = {}
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return '''
+    <h1>WebRTC Signaling Server</h1>
+    <p>Server is running successfully!</p>
+    <p>Current rooms: {}</p>
+    <p>Active users: {}</p>
+    '''.format(len(rooms), len(user_sessions))
 
 @socketio.on('connect')
 def handle_connect():
@@ -20,18 +31,23 @@ def handle_connect():
 def handle_disconnect():
     # Get user info from session if available
     user_id = request.sid
-    username = getattr(request, 'username', None)
-    room_id = getattr(request, 'room_id', None)
+    user_info = user_sessions.get(user_id)
     
-    if room_id and username:
+    if user_info:
+        username = user_info['username']
+        room_id = user_info['room_id']
+        
         # Remove user from room
         if room_id in rooms:
-            rooms[room_id] = {user for user in rooms[room_id] 
-                            if user['id'] != user_id}
+            rooms[room_id] = [user for user in rooms[room_id] 
+                            if user['id'] != user_id]
             
             # Remove room if empty
             if len(rooms[room_id]) == 0:
                 del rooms[room_id]
+        
+        # Remove user from sessions
+        del user_sessions[user_id]
         
         # Notify others in the room
         emit('user-left', {'username': username}, room=room_id)
@@ -46,17 +62,19 @@ def handle_join_room(data):
     user_id = request.sid
     
     # Store user info in session
-    request.username = username
-    request.room_id = room_id
+    user_sessions[user_id] = {
+        'username': username,
+        'room_id': room_id
+    }
     
     # Join the room
     join_room(room_id)
     
     # Add user to room tracking
     if room_id not in rooms:
-        rooms[room_id] = set()
+        rooms[room_id] = []
     
-    rooms[room_id].add({'id': user_id, 'username': username})
+    rooms[room_id].append({'id': user_id, 'username': username})
     
     # Notify others in the room
     emit('user-joined', {'username': username}, room=room_id, include_self=False)
@@ -65,8 +83,9 @@ def handle_join_room(data):
 
 @socketio.on('offer')
 def handle_offer(data):
-    room_id = getattr(request, 'room_id', None)
-    if room_id:
+    user_info = user_sessions.get(request.sid)
+    if user_info:
+        room_id = user_info['room_id']
         emit('offer', {
             'offer': data['offer'],
             'fromUser': data['fromUser']
@@ -74,8 +93,9 @@ def handle_offer(data):
 
 @socketio.on('answer')
 def handle_answer(data):
-    room_id = getattr(request, 'room_id', None)
-    if room_id:
+    user_info = user_sessions.get(request.sid)
+    if user_info:
+        room_id = user_info['room_id']
         emit('answer', {
             'answer': data['answer'],
             'fromUser': data['fromUser']
@@ -83,8 +103,9 @@ def handle_answer(data):
 
 @socketio.on('ice-candidate')
 def handle_ice_candidate(data):
-    room_id = getattr(request, 'room_id', None)
-    if room_id:
+    user_info = user_sessions.get(request.sid)
+    if user_info:
+        room_id = user_info['room_id']
         emit('ice-candidate', {
             'candidate': data['candidate'],
             'fromUser': data['fromUser']
